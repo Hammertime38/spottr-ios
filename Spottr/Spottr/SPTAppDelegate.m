08f66e89cdd825e2bd8ae4b486a9441c9f59fd37
John Hammerlund <johnhammerlund@gmail.com>
2014-08-08 20:53:59 -0700

    Add facebook stuff

--- Spottr/Spottr/SPTAppDelegate.m
+++ Spottr/Spottr/SPTAppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "SPTAppDelegate.h"
+#import <FacebookSDK/FBAppCall.h>
+#import <Parse/Parse.h>
 
 @implementation SPTAppDelegate
 
@@ -38,12 +40,19 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [FBAppCall handleDidBecomeActiveWithSession:[PFFacebookUtils session]];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    return [FBAppCall handleOpenURL:url
+                  sourceApplication:sourceApplication
+                        withSession:[PFFacebookUtils session]];
 }
 
 @end
