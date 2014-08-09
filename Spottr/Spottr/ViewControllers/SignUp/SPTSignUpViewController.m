1576979aa4dae9316137588554b4aa1cb43c37b9
Vignesh <vigneshk14@me.com>
2014-08-09 00:23:53 -0700

    Populated SignUpViewController

--- Spottr/Spottr/ViewControllers/SignUp/SPTSignUpViewController.m
+++ Spottr/Spottr/ViewControllers/SignUp/SPTSignUpViewController.m
@@ -7,12 +7,34 @@
 //
 
 #import "SPTSignUpViewController.h"
+#import <Parse/Parse.h>
 
 @interface SPTSignUpViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UIImageView *userImageView;
+@property (weak, nonatomic) IBOutlet UITextField *userFirstNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *userLastNameTextField;
 @end
 
 @implementation SPTSignUpViewController
+- (IBAction)startSpottingButtonTapped:(id)sender
+{
+    // TODO: Check validation
+    PFUser *newUser = [PFUser new];
+    [newUser setUsername:self.usernameTextField.text];
+    [newUser setPassword:self.passwordTextField.text];
+    [newUser setObject:self.userFirstNameTextField.text forKey:@"firstName"];
+    [newUser setObject:self.userLastNameTextField.text forKey:@"lastName"];
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if(succeeded) {
+            [self dismissViewControllerAnimated:YES completion:nil];
+        }
+        else {
+            NSLog(@"Failed to sign up new user: %@", error);
+        }
+    }];
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -26,7 +48,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    [self.userImageView.layer setCornerRadius:self.userImageView.frame.size.height/2];
+    [self.userImageView.layer setMasksToBounds:YES];
+    // TODO: Add image picker
 }
 
 - (void)didReceiveMemoryWarning
