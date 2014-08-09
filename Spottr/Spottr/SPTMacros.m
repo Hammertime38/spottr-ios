a61a9d111f7081febb271989e6b4117510921b6e
John Hammerlund <johnhammerlund@gmail.com>
2014-08-08 22:54:28 -0700

    Add more crap to .gitignore

---
+++ Spottr/Spottr/SPTMacros.m
@@ -0,0 +1,13 @@
+//
+//  SPTMacros.m
+//  Spottr
+//
+//  Created by John Hammerlund on 8/8/14.
+//  Copyright (c) 2014 Spottr. All rights reserved.
+//
+
+#import "SPTMacros.h"
+
+@implementation SPTMacros
+
+@end
