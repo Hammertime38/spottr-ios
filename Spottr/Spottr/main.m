745bd6f20d8e02712568c8e43efadf68c6d3ab0e
John Hammerlund <johnhammerlund@gmail.com>
2014-08-08 18:52:26 -0700

    Add boilerplate project setup

---
+++ Spottr/Spottr/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Spottr
+//
+//  Created by John Hammerlund on 8/8/14.
+//  Copyright (c) 2014 Spottr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SPTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SPTAppDelegate class]));
+    }
+}
---
+++ Spottr/SpottrTests/SpottrTests.m
@@ -0,0 +1,34 @@
+//
+//  SpottrTests.m
+//  SpottrTests
+//
+//  Created by John Hammerlund on 8/8/14.
+//  Copyright (c) 2014 Spottr. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SpottrTests : XCTestCase
+
+@end
+
+@implementation SpottrTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
