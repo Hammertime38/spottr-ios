cc93b82cee75c8e5ca7f30fe76d68a706cdec5ae
John Hammerlund <johnhammerlund@gmail.com>
2014-08-08 23:59:06 -0700

    Add blank custom workout table cell

---
+++ Spottr/Spottr/CustomCells/SPTWorkoutCellTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SPTWorkoutCellTableViewCell.m
+//  Spottr
+//
+//  Created by John Hammerlund on 8/8/14.
+//  Copyright (c) 2014 Spottr. All rights reserved.
+//
+
+#import "SPTWorkoutCellTableViewCell.h"
+
+@implementation SPTWorkoutCellTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
