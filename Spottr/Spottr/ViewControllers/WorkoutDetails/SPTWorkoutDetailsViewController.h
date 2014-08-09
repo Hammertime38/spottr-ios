494847b951e86b200b3db2d2e61b04c09d905725
John Hammerlund <johnhammerlund@gmail.com>
2014-08-08 19:38:29 -0700

    Add 'workout details' blank view controller

---
+++ Spottr/Spottr/ViewControllers/WorkoutDetails/SPTWorkoutDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPTWorkoutDetailsViewController.h
+//  Spottr
+//
+//  Created by John Hammerlund on 8/8/14.
+//  Copyright (c) 2014 Spottr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPTWorkoutDetailsViewController : UIViewController
+
+@end
---
+++ Spottr/Spottr/ViewControllers/WorkoutDetails/SPTWorkoutDetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  SPTWorkoutDetailsViewController.m
+//  Spottr
+//
+//  Created by John Hammerlund on 8/8/14.
+//  Copyright (c) 2014 Spottr. All rights reserved.
+//
+
+#import "SPTWorkoutDetailsViewController.h"
+
+@interface SPTWorkoutDetailsViewController ()
+
+@end
+
+@implementation SPTWorkoutDetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
