abd0afe44d4ad3608675ff03c6a090c9db1b357d
Vignesh <vigneshk14@me.com>
2014-08-09 03:13:45 -0700

    workoutcontrollerstuff uesfkdjhs ivhkdsjgh MY HANDS CAN TYPE WORDS

--- Spottr/Spottr/ViewControllers/CreateWorkout/SPTCreateWorkoutController.m
+++ Spottr/Spottr/ViewControllers/CreateWorkout/SPTCreateWorkoutController.m
@@ -13,6 +13,9 @@
 @property (weak, nonatomic) IBOutlet UITextField *activityTextField;
 @property (weak, nonatomic) IBOutlet UITextView *descriptionTextArea;
 @property (weak, nonatomic) IBOutlet UIDatePicker *dateTimePicker;
+@property (weak, nonatomic) IBOutlet UITextField *addressTextField;
+@property (weak, nonatomic) IBOutlet UIButton *spotButton;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UITextField *groupSizeTextField;
 @end
 
@@ -45,7 +48,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    [self.scrollView setContentSize:CGSizeMake(self.scrollView.frame.size.width, self.spotButton.frame.origin.y + self.spotButton.frame.size.height + 10)];
+    // magic numbers woo
 }
 
 - (void)didReceiveMemoryWarning
